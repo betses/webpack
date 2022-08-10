e26296046b2fe41d9559a6e5af6eef8472cae68c
Betsegaw Sebsibe <betsesebe@gmail.com>
2022-08-10 14:07:12 +0300

    Added addTask and removeTask functions

--- src/index.js
+++ src/index.js
@@ -1,42 +1,79 @@
-const todo = [
-  {
-    description: 'wash the dishes',
-    completed: true,
-    index: 5,
-  },
-  {
-    description: 'complete To Do list project',
-    completed: false,
-    index: 1,
-  },
-];
+// eslint-disable-next-line no-unused-vars
+import css from './app.css';
+import Tasks from './tasks/Task.js';
 
+const tasks = new Tasks();
 class List {
-  loadTodo = (parent) => {
-    todo.sort((a, b) => a.index - b.index);
+  handleEdit = (task, event) => {
+    tasks.editTask(task, event.target.value);
+  };
 
-    todo.forEach((task) => {
+  handleDelete = (task) => {
+    tasks.removeTask(task);
+
+    this.render();
+  };
+
+  handleCheckbox = (task, event) => {
+    const index = tasks.tasks.findIndex((t) => task.index === t.index);
+
+    if (index === -1) {
+      return;
+    }
+
+    tasks.tasks[index].completed = event.target.checked;
+
+    // @todo: replace the affected element only?
+    this.render();
+  };
+
+  handleTaskInput = (event) => {
+    if (event.key !== 'Enter') return;
+
+    tasks.addTask({
+      description: event.target.value,
+      completed: false,
+      index: tasks.tasks.length,
+    });
+
+    event.target.value = '';
+
+    this.render();
+  };
+
+  loadTodo(parent) {
+    const sortedTasks = tasks.getSortedTasks();
+
+    sortedTasks.forEach((task) => {
       const el = document.createElement('li');
+      el.setAttribute('tabindex', 0);
       const checkbox = document.createElement('input');
       checkbox.type = 'checkbox';
       checkbox.id = `task-${task.index}`;
       checkbox.addEventListener('change', (event) => this.handleCheckbox(task, event));
 
-      const label = document.createElement('label');
-      label.innerText = task.description;
-      label.setAttribute('for', `task-${task.index}`);
+      const input = document.createElement('input');
+      input.type = 'text';
+      input.value = task.description;
+      input.addEventListener('keyup', (event) => this.handleEdit(task, event));
+
+      const deleteButton = document.createElement('span');
+      deleteButton.classList.add('delete-button', 'material-symbols-outlined');
+      deleteButton.innerText = 'delete';
+      deleteButton.addEventListener('click', () => this.handleDelete(task));
 
       if (task.completed) {
         checkbox.setAttribute('checked', 'checked');
-        label.style.textDecoration = 'line-through';
-        label.style.color = '#070707';
+        input.style.textDecoration = 'line-through';
+        input.style.color = '#A3A3A3';
       }
 
       el.appendChild(checkbox);
-      el.appendChild(label);
+      el.appendChild(input);
+      el.appendChild(deleteButton);
       parent.appendChild(el);
     });
-  };
+  }
 
   render = () => {
     const parent = document.querySelector('#todo');
@@ -47,20 +84,13 @@ class List {
 
     this.loadTodo(parent);
   };
-
-  handleCheckbox = (task, event) => {
-    const index = todo.findIndex((t) => task.index === t.index);
-
-    if (index === -1) {
-      return;
-    }
-
-    todo[index].completed = event.target.checked;
-
-    this.render();
-  };
 }
 
+const list = new List();
+
 document.addEventListener('DOMContentLoaded', () => {
-  List.render();
+  document
+    .querySelector('#task-input')
+    .addEventListener('keyup', list.handleTaskInput);
+  list.render();
 });
